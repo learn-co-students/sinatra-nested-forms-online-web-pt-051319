41bf9ffb7e8f60d53a689e073a535698d4d2c5cd
jamievullo <jvullo@protonmail.com>
2019-08-10 17:04:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,11 +1,14 @@
 require './environment'
 
 module FormsLab
+  
   class App < Sinatra::Base
 
     # code other routes/actions here
-    get '/' do 
-      erb: index
+    # get '/' do 
+    #   erb: index
+    # end
+
   end
 
   
