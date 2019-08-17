b5119d509ca6d389c5a07789b33b41a99566a372
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-17 21:51:04 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@all = []
+
+  def inizialize(name,weight,height)
+    @name = name
+    @weight = weight
+    @height = height
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@all = []
+
+  def inizialize(name,type,booty)
+    @name = name
+    @type = type
+    @boot - booty
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
 end
