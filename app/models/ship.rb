a889454759d4982f0b7243b8da109711a567e9ea
ASHOK MAHARJAN <ashok997@gmail.com>
2019-08-20 04:20:39 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_reader :name, :type, :booty
+  
+  SHIPS = []
+  
+  def initialize()
+    @name = params[:name]
+    @type = params[:type]
+    @height = params[:booty]
+    SHIPS << self
+  end
+  
+  def self.all
+    SHIPS
+  end
+  
+  def self.clear
+    SHIPS.clear
+  end
 end
